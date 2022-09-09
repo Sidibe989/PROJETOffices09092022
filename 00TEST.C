43d922b4c9739715f980d66c779bd2ef653b8ed1
sidibe989 <saliousidibe69.sidibe@gmail.com>
2022-09-09 13:02:10 +0200

    secondo test

--- 00TEST.C
+++ 00TEST.C
@@ -1,6 +1,43 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    printf("hello");
-    return 0;
+  int numero, secondo ;
+  int somma=0;
+  int prodotto=0;
+  int resto=0;
+   printf("inserisce il primo numero ");
+   scanf("%d", &numero);
+   printf("inserisce il primo numero ");
+   scanf("%d", & secondo);
+   if (numero==secondo)
+   {
+    somma= numero+ secondo;
+     printf("%d", somma);  
+    /* code */
+   }
+   else{
+    if (numero<=0)
+    {
+     printf(" fai Attenzione che questo sistema lavoro solo numero reali ma positiva ");  
+    }
+    else{
+        if (numero==0)
+        {
+            /* code */
+            printf(" fai Attenzione che questo sistema lavoro solo partendo da 1 ");  
+        }
+        else{
+            if (numero==1)
+            {
+              prodotto=numero*secondo;
+               printf("%d", prodotto);  
+        }
+            }
+            
+        }
+        
+    }
+return 0;
 }
+    
+
